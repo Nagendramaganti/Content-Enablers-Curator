4b3d2f307a7898bec02b83021e0569983fae258d
nagendra <nagendra@contentenablers.com>
2019-07-20 07:14:17 +0530

    second commit

--- Java practice/src/org/strings/GitDemo.java
+++ Java practice/src/org/strings/GitDemo.java
@@ -2,7 +2,28 @@ package org.strings;
 
 public class GitDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  //for array sorting using java selection sort
+	{
+		int a[] = {4,6,3,5,8,1};
+		for(int i=0;i<a.length;i++)
+		{
+			int temp;
+			for(int j=i+1;j<a.length;j++)
+				
+			{
+				if(a[i]>a[j])
+				{
+					temp=a[i];
+					a[i]=a[j];
+					a[j]= temp;
+				}
+			}
+			
+		}
+		for(int k=0;k<a.length;k++)
+		{
+			System.out.print(a[k]);
+		}
 		System.out.print("this is git --- editted in git oonline");
 
 	}
